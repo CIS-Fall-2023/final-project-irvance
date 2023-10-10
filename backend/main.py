94c3e5aaf1db117ce554f2f6eacc1ccdbad0cf70
irvance <98298252+irvance@users.noreply.github.com>
2023-10-10 18:13:42 -0500

    Update main.py
    added database connection + read queries for each table

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,25 @@
 # final project part 1
+import flask
+from flask import jsonify
+from flask import request
+import creds
+from sql import create_connection
+from sql import execute_read_query
+from sql import execute_query
+
+#objective: GET: Return ALL animals from zoo; POST: add new animal to zoo; PUT: update column given an id; DELETE: delete an animal, given an id
+app = flask.Flask(__name__) # sets up application
+app.config["DEBUG"] = True # allows errors to show in browser
+
+myCreds = creds.Creds()
+conn = create_connection(myCreds.host, myCreds.user, myCreds.password, myCreds.database)
+
+floor= "SELECT * FROM floor"
+sql_floor = execute_read_query(conn,floor)
+
+room = "SELECT * FROM room"
+sql_room = execute_read_query(conn,room)
+
+resident = "SELECT * FROM resident"
+sql_resident = execute_read_query(conn,resident)
+
