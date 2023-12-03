0e3bd96606ff937428f39624d2b8c16657030922
irvance <98298252+irvance@users.noreply.github.com>
2023-12-02 20:49:22 -0600

    added login page
    added login page, created index,

---
+++ frontend/server.js
@@ -0,0 +1,47 @@
+// load the things we need
+var express = require('express');
+var app = express();
+const bodyParser  = require('body-parser');
+
+// required module to make calls to a REST API
+const axios = require('axios');
+
+app.use(bodyParser.urlencoded());
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+     res.render("pages/index.ejs", {});
+    //  res.render("pages/alias.ejs", {});
+});
+
+//instead of checking if user = admin, a
+  app.post('/process_login', function(req, res){
+    var user = req.body.username;
+    var password = req.body.password;
+
+    if(user === 'admin' && password === 'password') // '===': triple equal checks if the object type is the same between to objects; ex: 1 vs '1'
+    {
+        res.render('pages/welcome.ejs', {
+            user: user,
+            auth: true
+        });
+    }
+    else
+    {
+        res.render('pages/welcome.ejs', {
+            user: 'UNAUTHORIZED',
+            auth: false
+        });
+    }
+  })
+
+  app.post('/')
+
+
+app.listen(8080);
+console.log('8080 is the magic port');
